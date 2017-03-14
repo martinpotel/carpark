d37c1c273e11cfdfdbe7f5284cd63c1ce0e39e3d
martinpotel <mrtn.potel@gmail.com>
2017-03-14 16:13:16 +0000

    add form login register

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var rgeApp = angular.module('rgeApp', ['ngRoute', 'ngSanitize', 'ngCookies', 'ngMaterial', 'homeModule', 'noteModule']);
+var rgeApp = angular.module('rgeApp', ['ngRoute', 'ngSanitize', 'ngCookies', 'ngMaterial', 'homeModule', 'noteModule', 'loginModule', 'registerModule']);
 
 /* Angular material theme */
 rgeApp.config(function($mdThemingProvider) {
